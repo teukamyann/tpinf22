f6b5743348098927aa94d70059e638f67ebe59ba
teukamyann <yannmarie2007@gmail.com>
2026-03-23 02:52:19 +0100

    article.model.js

--- article.model.js
+++ article.model.js
@@ -1,14 +1,14 @@
-// Structure d'un article selon le sujet [cite: 45]
-class Article {
-    constructor(id, titre, contenu, auteur, categorie, tags) {
-        this.id = id;
-        this.titre = titre;
-        this.contenu = contenu;
-        this.auteur = auteur; // Obligatoire [cite: 101]
-        this.categorie = categorie;
-        this.tags = tags;
-        this.date = new Date().toISOString();
-    }
-}
+const fs = require('fs');
+const FILE = './articles.json';
 
-module.exports = Article;
+const lire = () => {
+    if (!fs.existsSync(FILE)) return [];
+    return JSON.parse(fs.readFileSync(FILE, 'utf-8') || "[]");
+};
+
+const sauvegarder = (data) => {
+    fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
+};
+
+// C'est cette ligne qui manquait ou qui bloquait :
+module.exports = { lire, sauvegarder };
